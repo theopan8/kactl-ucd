2e3bb029540ece3083d74d1674dffae8c6e70cb7
Theodore Pan <77665596+theopan8@users.noreply.github.com>
2023-05-24 20:45:57 -0700

    Update FenwickTree.h

--- content/data-structures/FenwickTree.h
+++ content/data-structures/FenwickTree.h
@@ -10,25 +10,19 @@
  */
 #pragma once
 
-struct FT {
-	vector<ll> s;
-	FT(int n) : s(n) {}
-	void update(int pos, ll dif) { // a[pos] += dif
-		for (; pos < sz(s); pos |= pos + 1) s[pos] += dif;
-	}
-	ll query(int pos) { // sum of values in [0, pos)
-		ll res = 0;
-		for (; pos > 0; pos &= pos - 1) res += s[pos-1];
-		return res;
-	}
-	int lower_bound(ll sum) {// min pos st sum of [0, pos] >= sum
-		// Returns n if no sum is >= sum, or -1 if empty sum is.
-		if (sum <= 0) return -1;
-		int pos = 0;
-		for (int pw = 1 << 25; pw; pw >>= 1) {
-			if (pos + pw <= sz(s) && s[pos + pw-1] < sum)
-				pos += pw, sum -= s[pos-1];
-		}
-		return pos;
-	}
-};
+const int MAXN = 100010;
+int bit[MAXN];
+int query(int idx){
+    int sum = 0;
+    while(idx > 0){
+        sum += bit[idx];
+        idx -= (idx & -idx);
+    }
+    return sum;
+}
+void update(int idx, int val){
+    while(idx < MAXN){
+        bit[idx] += val;
+        idx += (idx & -idx);
+    }
+}
